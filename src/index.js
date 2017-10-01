0317ef23065e2e91fe92fa3b9caa647627f7927d
albert <albert.cymbaluk@gmail.com>
2017-10-01 21:35:46 +0200

    new with problem

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducer } from './reducer.js';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import { addComment } from './actions.js';
+import DevTools from './DevTools';
+
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+store.dispatch(addComment('pierwszy komentarz'));
+store.dispatch(addComment('drugi komentarz'));
+
+
