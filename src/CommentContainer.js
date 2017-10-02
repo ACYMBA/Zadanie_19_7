8b90f7c43cb3b0ca6c827c845c8096901aa938bc
albert <albert.cymbaluk@gmail.com>
2017-10-02 23:26:52 +0200

    correction

--- src/CommentContainer.js
+++ src/CommentContainer.js
@@ -1,10 +1,6 @@
 import {connect} from 'react-redux';
 import Comment from './Comment';
-<<<<<<< HEAD
 import {thumbUpComment} from './actions';
-=======
-import {thumbUpComment} from './actions'
->>>>>>> 0b41355c5a79fe028efa0a774589bae026288227
 
 const mapDispatchToProps = dispatch => ({
   thumbUpComment: (id) => dispatch(thumbUpComment(id))
--- src/CommentsList.js
+++ src/CommentsList.js
@@ -1,8 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
 
-=======
->>>>>>> 0b41355c5a79fe028efa0a774589bae026288227
 import Comment from './CommentContainer';
 
 const CommentsList = ({comments}) => <ul>{comments.map(comment => <Comment key={comment.id} {...comment}/>)}</ul>;
